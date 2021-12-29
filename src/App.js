c347a8fdb8e964686bd6a7b975a3515a33de348f
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-12-29 14:38:27 +0300

    research components functions

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ import VerifyEmail from "./components/VerifyAccount";
 import Profile from "./components/Profile";
 import ForgetPass from "./components/ForgetPass";
 import ResetPass from "./components/ResetPass";
+import Researches from "./components/Researches";
+import OneResearch from "./components/OneResearch";
+// import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
+      {/* <Navbar> */}
       <Routes>
         <Route exact path="/newSpicalist" element={<NewSpecialist />} />
         <Route exact path="/newUser" element={<NewUser />} />
@@ -18,8 +22,11 @@ const App = () => {
         <Route exact path="/verifyEmail/:token" element={<VerifyEmail />} />
         <Route exact path="/profile" element={<Profile />} />
         <Route exact path="/forgetPass" element={<ForgetPass />} />
-        <Route exact path="/resetPass/:id" element={<ResetPass />} />
+        <Route exact path="/resetPass/:id" element={<ResetPass />} />{" "}
+        <Route exact path="/researches" element={<Researches />} />
+        <Route exact path="/research/:id" element={<OneResearch />} />
       </Routes>
+      {/* </Navbar> */}
     </>
   );
 };
